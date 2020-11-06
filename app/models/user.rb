b66b3391446a624d11fb125001db61c6f61b278d
”Iizuka” <yutorehit1209@icloud.com>
2020-11-06 16:23:23 +0000

    ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true,length: {maximum: 15}
   validates :email, presence: true
+  validates :password, length: {maximum: 32, minimum: 8}
+  validates :password, format: {with:/[0-9a-z]+/}
+  validates :email, format: {with:/[0-9a-zA-Z]+@[a-z]+.[a-z]+/}
+  has_secure_password
 end
